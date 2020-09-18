f14546b9164c1ff1d03d48d8c39a66a388839832
euberdeveloper <euberdeveloper@gmail.com>
2020-09-18 20:56:47 +0200

    feature: ready to be published

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'scell',
+  packages = ['scell'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'A framework made with python for C cli projects',
+  author = 'Eugenio Vinicio Berretta',
+  author_email = 'euberdeveloper@gmail.com'
+  url = 'https://github.com/euberdeveloper/euberdeveloper',
+  download_url = 'https://github.com/euberdeveloper/scell/archive/v_0.0.1.tar.gz',
+  keywords = ['scell', 'shell', 'c', 'framework'],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
